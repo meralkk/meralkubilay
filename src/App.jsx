ad0b74db8ed64c726b8238266a77884412e48616
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2024-03-01 14:06:49 +0100

    created the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Footer from '../components/footer/footer'
+import Header from '../components/header/header'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <Footer/>
+
+    </div>
   )
 }
 
