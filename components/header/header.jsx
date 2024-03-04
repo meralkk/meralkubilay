113daf2773bb365f0e89357e0f14179f2af97866
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2024-03-04 18:43:19 +0100

    created pages

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,25 +1,9 @@
-import React from 'react';
-import { Container, Navbar, Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './header.scss'
+import React from 'react'
 
 const Header = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid className='navbar-container'>
-        <Navbar.Brand className='logo' href="#home">Meral.dev</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="nav-link ms-auto"> 
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About Me</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>Header</div>
   )
 }
 
-export default Header;
+export default Header
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-// App.js veya App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../pages/home/home';
-import About from '../pages/about/about';
-import Projects from '../pages/projects/projects';
-import Contact from '../pages/contact/contact';
-import Header from '../components/header/header';
-import Footer from '../components/footer/footer';
+import React from 'react'
+import Footer from '../components/footer/footer'
+import Header from '../components/header/header'
 
-function App() {
+const App = () => {
   return (
     <div>
-      <Header />
-      <main>
-        <Home />
-        <About />
-        <Projects />
-        <Contact />
-      </main>
+      <Header/>
+      <Footer/>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
